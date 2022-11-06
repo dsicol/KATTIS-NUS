ce2e01d6119106030aaeb96a670448b88471a3d8
Darren Leo <116002950+dsicol@users.noreply.github.com>
2022-11-06 16:10:30 +0800

    Create Single_Source_Shortest_Path_Safe_Path.cpp

---
+++ Single_Source_Shortest_Path_Safe_Path.cpp
@@ -0,0 +1,99 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+
+int main() {
+
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+    int TC; cin >> TC;
+
+    while (TC--) {
+        
+        int V; cin >> V;
+        vector<vii> AL(V, vii());
+
+        for (int u = 0; u < V; ++u) {
+
+            int X; cin >> X; // Edges
+
+            while (X--) {
+                
+                int vertex, weight; cin >> vertex >> weight;
+                AL[u].emplace_back(vertex, weight);
+
+            }
+
+        }
+
+        int Q; cin >> Q;
+
+        while (Q--) {
+
+            int s, t, k; cin >> s >> t >> k;
+            int INF = (int)1e9;
+
+            vi dist(V, INF); dist[s] = 0;
+            vi junctions(V,0); junctions[s] = 1;
+                            
+            for (int i = 0; i < V - 1; i++) {  
+
+                bool modified = false; 
+                bool k1 = false;   
+
+                for (int u = 0; u < V; u++) {
+
+                    if (dist[u] != INF) {
+
+                        for (auto &[v, w] : AL[u]) {       
+
+                            if (dist[u] + w >= dist[v]) {
+                                continue;   
+                            }
+
+                            int currjunction = 1 + junctions[u];
+
+                            if (k == 1 && t != s) {
+
+                                k1 = true;
+                                break;
+
+                            } else if (currjunction < k) { // process k - 1 edges
+
+                                dist[v] = dist[u] + w;  
+                                junctions[v] = currjunction;
+                                modified = true; 
+                                
+                            } else { 
+
+                                if (v == t) { // if t, process last edge, else do nothing (INF)
+
+                                    dist[v] = dist[u] + w;
+                                    ++junctions[v];
+                                    modified = true; 
+
+                                }
+
+                            }
+                           
+                        }
+
+                    }
+
+                }  
+
+                if (!modified || k1) break;  
+
+            }
+
+            cout << ((dist[t] != INF) ? dist[t] : -1) << '\n';
+            
+        }
+
+    }
+
+    return 0;
+
+}
