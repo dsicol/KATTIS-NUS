51c58fb8f4805ce664d77ea2c790ce194d0507aa
Darren Leo <116002950+dsicol@users.noreply.github.com>
2023-03-07 23:14:11 +0800

    Edit code style

--- Single_Source_Shortest_Path_Safe_Path.cpp
+++ Single_Source_Shortest_Path_Safe_Path.cpp
@@ -14,18 +14,14 @@ int main() {
         
         int V; cin >> V;
         vector<vii> AL(V, vii());
-
+        
         for (int u = 0; u < V; ++u) {
-
             int X; cin >> X; // Edges
-
             while (X--) {
-                
                 int vertex, weight; cin >> vertex >> weight;
                 AL[u].emplace_back(vertex, weight);
-
             }
-
+            
         }
 
         int Q; cin >> Q;
@@ -39,51 +35,33 @@ int main() {
             vi junctions(V,0); junctions[s] = 1;
                             
             for (int i = 0; i < V - 1; i++) {  
-
                 bool modified = false; 
-
                 for (int u = 0; u < V; u++) {
-
                     if (dist[u] != INF) {
-
                         for (auto &[v, w] : AL[u]) {       
-
                             if (dist[u] + w >= dist[v]) {
-
                                 continue;   
-
                             }
 
                             int currjunction = 1 + junctions[u];
 
                             if (k == 1 && t != s) {
-
                                 break;
-
                             } else if (currjunction < k || v == t) { // process k - 1 edges
-
                                 dist[v] = dist[u] + w;  
                                 junctions[v] = currjunction;
-                                modified = true; 
-                                
-                            } 
-                           
+                                modified = true;    
+                            }                           
                         }
-
                     }
-
                 }  
-
                 if (!modified) break;  
-
             }
-
             cout << ((dist[t] != INF) ? dist[t] : -1) << '\n';
-
+            
         }
 
     }
-
     return 0;
 
 }
