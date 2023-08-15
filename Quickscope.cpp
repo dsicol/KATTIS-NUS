3a4e12d46c1505f40205bb947dfae6fda541c143
Darren Leo <116002950+dsicol@users.noreply.github.com>
2023-08-15 21:58:52 +0800

    Optimise code

--- Quickscope.cpp
+++ Quickscope.cpp
@@ -2,47 +2,40 @@
 using namespace std;
 
 int main() {
-
     ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-
     int N; cin >> N;
-    int scope = 0;
-
-    vector< unordered_map<string, vector<string>> > variables; // DS segmented by indices of scope to isolate multiple declarations.
-    unordered_map<string, vector<string>> lastDeclared; // DS to keep track of last declared variables irregardless of scope. The use of a vector is faster than using a Stack.
-    unordered_map<string, vector<string>> line;
+  
+    vector<unordered_set<string>> variables; // DS segmented by indices of scope to isolate multiple declarations.
+    unordered_map<string, vector<string>> types; // DS to keep track of last declared variables irregardless of scope. The use of a vector is faster than using a Stack.
+    unordered_set<string> st;
     
-    variables.push_back(line);
+    variables.push_back(st);
 
     while (N--) {
         string token;
         cin >> token;
 
         if (token == "{") {
-            ++scope;
-            variables.push_back(line);
-
+            variables.push_back(st);
+            
         } else if (token == "}") {
-            for (auto i : variables.back()) {
-                if (!lastDeclared[i.first].empty()) lastDeclared[i.first].pop_back(); // Avoid segmentation fault
+            for (auto& i : variables.back()) {
+                if (!types[i].empty()) types[i].pop_back(); // Avoid segmentation fault
             }
-            --scope;
             variables.pop_back();
             
         } else if (token == "DECLARE") {
             string variable, type; cin >> variable >> type;
-            variables[scope][variable].push_back(type); lastDeclared[variable].push_back(type);
-
-            if (variables[scope][variable].size() > 1) {
+            if (variables.back().find(variable) != variables.back().end()) {
                 cout << "MULTIPLE DECLARATION" << '\n';
                 return 0;
             }
-
-        } else if (token == "TYPEOF") {
+            variables.back().insert(variable); types[variable].push_back(type);
+            
+        } else {
             string variable;
             cin >> variable;
-
-            (!lastDeclared[variable].empty()) ? cout << lastDeclared[variable].back() << '\n' : cout << "UNDECLARED" << '\n';
+            (!types[variable].empty()) ? cout << types[variable].back() << '\n' : cout << "UNDECLARED" << '\n';
         }
     }
     return 0;
